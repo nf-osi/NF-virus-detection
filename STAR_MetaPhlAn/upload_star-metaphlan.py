160efed12374d470a396ae5204c3b532c29fe249
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2021-08-23 01:53:44 -0700

    Create upload_star-metaphlan.py

---
+++ STAR_MetaPhlAn/upload_star-metaphlan.py
@@ -0,0 +1,25 @@
+import os
+import synapseclient
+from synapseclient import File, Folder
+
+PROJECT = 'syn25992642'
+START_PATH = '/home/ssm-user/STAR_MetaPhlAn/analysis/star_results/'
+syn=synapseclient.login('username','password')
+
+
+parents = {START_PATH: PROJECT}
+for dirpath, dirnames, filenames in os.walk(START_PATH):
+    #Add the subfolders
+    for dirname in dirnames:
+        print( 'creating', dirname)
+        f = syn.store(Folder(dirname, parent = parents[dirpath]))
+        parents[os.path.join(dirpath, dirname)] = f.id
+    #Add the files
+    for name in filenames:
+        path = os.path.join(dirpath, name)
+        if os.stat(path).st_size>0:
+            print( 'uploading', path)
+            f = File(path, parent = parents[dirpath], name=name)
+            #f.fileType = name[-3:]
+            #f.project = 'Cool imaging library
+            syn.store(f, forceVersion=False)
