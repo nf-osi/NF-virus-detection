904556246a4759988bd01ce0ad491e95f64eecc3
Robert Allaway <allaway@users.noreply.github.com>
2022-08-18 12:08:00 -0700

    Update VIF.sh

--- VIF.sh
+++ VIF.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-FILES="/home/ssm-user/data/organoid/"*"_R1_"*".fastq.gz"
+#FILES="/home/ssm-user/data/organoid/"*"_R1_"*".fastq.gz"
 #FILES="/home/ssm-user/data/CTF/*_1.fastq"
+#FILES=/home/ssm-user/data/cnf_cells/*_1.fastq"
+
 for f in $FILES
 do
         echo "Processing $f file..."
@@ -12,7 +14,7 @@ do
         substring=$(echo $file1| cut -d'R' -f 1)
         echo $substring
 
-        file2=$substring"R2_001.fastq.gz"
+        file2=$substring"R2.fastq.gz"
         echo $file2
 
         dirname=${substring::-1}
@@ -20,8 +22,8 @@ do
 
         docker run -v `pwd`:/ssm-user --rm trinityctat/ctat_vif \
                 /usr/local/bin/ctat-VIF.py \
-                --left_fq /ssm-user/data/organoid/$file1 \
-                --right_fq /ssm-user/data/organoid/$file2 \
+                --left_fq /ssm-user/data/cnf_cells/$file1 \
+                --right_fq /ssm-user/data/cnf_cells/$file2 \
                 --genome_lib_dir /ssm-user/VIF/GRCh37_gencode_v19_CTAT_lib_Mar012021.plug-n-play/ctat_genome_lib_build_dir \
                 --viral_db_fasta /ssm-user/VIF/viruses.fasta \
                 --output_dir /ssm-user/VIF/results/$dirname/ \
