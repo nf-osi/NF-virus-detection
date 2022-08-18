1177c12fcc4af40c6475454265c03457e29a1305
Robert Allaway <allaway@users.noreply.github.com>
2022-08-18 12:07:11 -0700

    Update create_genome_index.sh

--- create_genome_index.sh
+++ create_genome_index.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-sudo mkdir /home/ssm-user/analysis/genome_index
-cd /home/ssm-user/analysis/genome_index
-sudo wget http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.dna.primary_assembly.fa
-sudo wget http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.99.gtf
-sudo chmod -R 0777 .
+!/bin/bash
+mkdir $HOME/analysis/
+mkdir $HOME/analysis/genome_index
+cd $HOME/analysis/genome_index
+wget http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+wget http://labshare.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARgenomes/Human/GRCh38_Ensembl99_sparseD3_sjdbOverhang99/Homo_sapiens.GRCh38.99.gtf
+sudo chmod -R 0755 .
 
-docker run -v `pwd`:/ssm-user --rm nfosi/star_metaphlan \
+docker run -v $HOME:/ssm-user --rm nfosi/star_metaphlan \
                 STAR --runThreadN 16 --runMode genomeGenerate \
                 --genomeDir /ssm-user/analysis/genome_index/index \
                 --genomeFastaFiles /ssm-user/analysis/genome_index/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
